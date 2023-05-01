52a1a473fcba5cd3379f6e1db48301bc8c6208f8
dzenbro2020 <ilin.bk@yandex.ru>
2023-05-01 23:31:17 +0300

    create new pages and some tests

--- first/first/settings.py
+++ first/first/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages', #new
+    'pages', #new  
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'first.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))], #new
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
